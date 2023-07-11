0090920f00075b5375c199c074e935eca25d836a
Alexander Pozas <a.posas@outlook.com>
2023-07-10 22:53:36 -0600

    10jul Cambios en la validación del formulario de contacto

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import smoothScrolling from "./smoothScrolling.js";
 import hamburguerMenu from "./hamburguerMenu.js";
-import validateInput from "./validator.js";
+import validateInput, { validInputs } from "./validator.js";
 
 const form = document.querySelector('.form');
 const submitButtom = document.querySelector('[data-submit-btn]');
@@ -9,32 +9,27 @@ const inputArea = document.querySelector('textarea');
 
 
 
-document.addEventListener('DOMContentLoaded', () => {    
+document.addEventListener('DOMContentLoaded', () => {
     iniciarApp();
     smoothScrolling();
     hamburguerMenu();
-
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        console.log('Enviando formulario');
-    })
 
     formInputs.forEach(input => {
         input.addEventListener("blur", (e) => {
             validateInput(e.target);
         });
     });
-
+    
     inputArea.addEventListener("blur", (e) => validateInput(e.target));
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        console.log('Enviando formulario');
+    })
 })
 
 function iniciarApp() {
     submitButtom.disabled = true;
     submitButtom.classList.add('not-allowed');
 }
-
-
-
-
-
 
--- src/js/validator.js
+++ src/js/validator.js
@@ -31,7 +31,6 @@ const validators = {
 };
 
 const validateInput = (input) => {
-
     if (validators[input.id]) {
         validators[input.id](input);
     }
