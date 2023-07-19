16b29db7f87863c57b5856b2c7189ddd8cf95b28
Alexander Pozas <a.posas@outlook.com>
2023-07-18 22:13:46 -0600

    Agregando AOS-Animation On Scroll al proyecto

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,18 @@ const form = document.querySelector('.form');
 const formInputs = document.querySelectorAll('input');
 const inputArea = document.querySelector('textarea');
 
+const educationCards = document.querySelectorAll('.education-card');
+const projects = document.querySelectorAll('.experience');
+const experienceImages = document.querySelectorAll('.experience__image');
+const experienceContents = document.querySelectorAll('.experience__content');
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    // iniciarApp();
+    AOS.init({
+        duration: 1500,
+        offset: 150,
+    });
     smoothScrolling();
     hamburguerMenu();
 
@@ -24,6 +31,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        console.log('Enviando formulario');
+        console.log('Enviando Formulario con éxito');
     })
 })
+
+educationCards.forEach( card => {
+    card.dataset.aos = 'fade-up';
+})
+
+for(let i = 0; i < projects.length; i++) {
+    if(i % 2 === 0){
+        experienceImages[i].dataset.aos = 'fade-up-mq-right';
+        experienceContents[i].dataset.aos = 'fade-up-mq-left';
+
+    }else {
+        experienceImages[i].dataset.aos = 'fade-up-mq-left';
+        experienceContents[i].dataset.aos = 'fade-up-mq-right';
+    }
+}
+
